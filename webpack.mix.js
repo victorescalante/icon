674e7351b4e5b612dff82d75c99acb4087b4609f
Victor Escalante <victorlt.xd3@gmail.com>
2021-05-17 16:07:27 -0500

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'dist/bootstrap.css')
-mix.sass('src/app.scss', 'dist');
+mix.sass('src/app.scss', 'dist/app.1.css');
